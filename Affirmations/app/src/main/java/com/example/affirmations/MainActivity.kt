fb980ccb5abbe6e218aa2446a7ecd1a45e66028a
luci649 <125581198+luci649@users.noreply.github.com>
2024-04-28 16:04:56 +0100

    Added card composable then in that added column composable to provide a vertical layout for the image and text composables that are going to be displayed the image and corresponding affirmation.

--- Affirmations/app/src/main/java/com/example/affirmations/MainActivity.kt
+++ Affirmations/app/src/main/java/com/example/affirmations/MainActivity.kt
@@ -3,13 +3,26 @@ package com.example.affirmations
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.affirmations.ui.theme.AffirmationsTheme
+import com.example.model.Affirmation
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.platform.LocalContext
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,12 +46,33 @@ fun MainPage(modifier: Modifier = Modifier) {
 
 }
 
+@Composable
+fun AffirmationCard(affirmation: Affirmation, modifier: Modifier = Modifier){
+    Card(modifier = modifier) {
+        Column {
+            Image(
+                painter = painterResource(affirmation.imageResourceId),
+                contentDescription = stringResource(affirmation.stringResourceId),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(194.dp),
+                contentScale = ContentScale.Crop
+            )
+            Text(
+                text = LocalContext.current.getString(affirmation.stringResourceId),
+                modifier = Modifier.padding(16.dp),
+                style = MaterialTheme.typography.headlineSmall
+            )
+        }
+    }
+}
+
 @Preview(showBackground = true,
-    showSystemUi = true,
+    showSystemUi =   true,
     name = "My Preview")
 @Composable
-fun AffirmationsPreview() {
+private fun AffirmationsCardPreview() {
     AffirmationsTheme {
-        MainPage()
+
     }
 }
