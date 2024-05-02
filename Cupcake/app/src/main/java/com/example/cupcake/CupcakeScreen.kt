79a942a432e41d83b37531ad53c72366ace1bd87
luci649 <125581198+luci649@users.noreply.github.com>
2024-05-02 21:01:46 +0100

    Added functionality for sending the order summary screen to other apps.

--- Cupcake/app/src/main/java/com/example/cupcake/CupcakeScreen.kt
+++ Cupcake/app/src/main/java/com/example/cupcake/CupcakeScreen.kt
@@ -15,6 +15,8 @@
  */
 package com.example.cupcake
 
+import android.content.Context
+import android.content.Intent
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
@@ -46,6 +48,7 @@ import androidx.compose.ui.platform.LocalContext
 import com.example.cupcake.ui.SelectOptionScreen
 import com.example.cupcake.data.OrderUiState
 import com.example.cupcake.ui.OrderSummaryScreen
+import javax.security.auth.Subject
 
 enum class CupcakeScreen(){
     Start,
@@ -137,11 +140,12 @@ fun CupcakeApp(
                 )
             }
             composable(route = CupcakeScreen.Summary.name){
+                val context = LocalContext.current
                 OrderSummaryScreen(
                     orderUiState = uiState,
                     onCancelButtonClicked = { cancelOrderAndNavigateToStart(viewModel, navController) },
                     onSendButtonClicked = { subject: String, summary: String ->
-
+                        shareOrder(context, subject = subject, summary = summary)
                     },
                     modifier = Modifier.fillMaxHeight()
                 )
@@ -156,4 +160,18 @@ private fun cancelOrderAndNavigateToStart(
 ){
     viewModel.resetOrder()
     navController.popBackStack(CupcakeScreen.Start.name, inclusive = false)
+}
+
+private fun shareOrder(context: Context, subject: String, summary: String){
+    val intent = Intent(Intent.ACTION_SEND).apply{
+        type = "text/plain"
+        putExtra(Intent.EXTRA_SUBJECT, subject)
+        putExtra(Intent.EXTRA_TEXT, summary)
+    }
+    context.startActivities(
+        Intent.createChooser(
+            intent,
+            context,getString(R.string.new_cupcake_order)
+        )
+    )
 }
