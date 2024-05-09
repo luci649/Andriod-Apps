c38fe5fa2e99d8589eb1ab092003b726dad39e94
luci649 <125581198+luci649@users.noreply.github.com>
2024-05-09 21:32:25 +0100

    Added log tag to identify when the activity entered the Start state. But that is after overriding the onStart function and calling the original function.

--- DessertClicker/app/src/main/java/com/example/dessertclicker/MainActivity.kt
+++ DessertClicker/app/src/main/java/com/example/dessertclicker/MainActivity.kt
@@ -82,6 +82,11 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart Called")
+    }
 }
 
 /**
