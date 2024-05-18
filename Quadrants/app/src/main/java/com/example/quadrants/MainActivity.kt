38640324b8905f233b3d4ec73ed222c8e64400ae
luci649 <125581198+luci649@users.noreply.github.com>
2024-05-18 20:33:42 +0100

    Imported multiple different flags and added the name of the flags as the title.

--- Quadrants/app/src/main/java/com/example/quadrants/MainActivity.kt
+++ Quadrants/app/src/main/java/com/example/quadrants/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.quadrants
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -16,7 +17,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -46,28 +50,28 @@ fun MainPage(modifier: Modifier = Modifier) {
     Column(Modifier.fillMaxWidth()) {
         Row (Modifier.weight(1f)){
             Quadrant(
-                title = stringResource(R.string.first_title),
-                description = stringResource(R.string.first_description),
+                flag = painterResource(R.drawable.flag_of_belgium_svg),
+                title = stringResource(R.string.belgium_flag_title),
                 background = colorResource(R.color.quadrant_1_bg),
                 modifier = Modifier.weight(1f)
             )
             Quadrant(
-                title = stringResource(R.string.second_title),
-                description = stringResource(R.string.second_description),
+                flag = painterResource(R.drawable._200px_flag_of_malaysia),
+                title = stringResource(R.string.malaysia_flag_title),
                 background = colorResource(R.color.quadrant_2_bg),
                 modifier = Modifier.weight(1f)
             )
         }
         Row (Modifier.weight(1f)){
             Quadrant(
-                title = stringResource(R.string.third_title),
-                description = stringResource(R.string.third_description),
+                flag = painterResource(R.drawable.download__2_),
+                title = stringResource(R.string.tanzania_flag_title),
                 background = colorResource(R.color.quadrant_3_bg),
                 modifier = Modifier.weight(1f)
             )
             Quadrant(
-                title = stringResource(R.string.fourth_title),
-                description = stringResource(R.string.fourth_description),
+                flag = painterResource(R.drawable.download__1_),
+                title = stringResource(R.string.Peru_flag_title),
                 background = colorResource(R.color.quadrant_4_bg),
                 modifier = Modifier.weight(1f)
             )
@@ -78,8 +82,8 @@ fun MainPage(modifier: Modifier = Modifier) {
 
 @Composable
 fun Quadrant(
+    flag: Painter,
     title: String,
-    description: String,
     background: androidx.compose.ui.graphics.Color,
     modifier: Modifier = Modifier
 ){
@@ -91,17 +95,17 @@ fun Quadrant(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ){
+        Image(
+            painter = flag,
+            contentDescription = title,
+            contentScale = ContentScale.Fit,
+            modifier = modifier.padding(top = 50.dp)
+        )
         Text(
             text = title,
             textAlign = TextAlign.Justify,
             fontWeight = FontWeight.Bold,
-            modifier = modifier.padding(bottom = 16.dp, top = 120.dp)
-        )
-        Text(
-            text = description,
-            textAlign = TextAlign.Justify,
-            modifier = modifier.padding(bottom = 55.dp
-            )
+            modifier = modifier.padding(bottom = 16.dp)
         )
     }
 }
